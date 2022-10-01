114332e5dfbe3f2cfa16741d1676e5ab86e52cdc
irshathahamed21 <21irshathkhan@gmail.com>
2022-10-01 16:10:44 +0530

    Search component done

---
+++ frontend/src/components/product/Search.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+import "./search.css"
+
+
+const Search = ({history}) => {
+  const[keyword, setKeyword] = useState("")
+
+  const searchSubmitHandler = (e) => {
+    e.preventDefault()
+
+    if(keyword.trim()){
+      history.push(`/products/${keyword}`)
+    }
+    else {
+      history.push("/products")
+    }
+
+  }
+
+
+  return (
+    <>
+    <form className = "searchBox" onSubmit={searchSubmitHandler}>
+      <input  type="text"
+            placeholder='Search a Product'
+            onChange = {(e) => setKeyword(e.target.value)} />
+      <input type = "submit" value = "Search"/>
+    </form>
+    </>
+  )
+}
+
+export default Search
