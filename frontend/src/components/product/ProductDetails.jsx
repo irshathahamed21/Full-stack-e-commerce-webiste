07fefebc37558eb12a36a0a3ad81cde3d9d65545
irshathahamed21 <21irshathkhan@gmail.com>
2022-10-01 16:11:53 +0530

    added reviews for the product

--- frontend/src/components/product/ProductDetails.jsx
+++ frontend/src/components/product/ProductDetails.jsx
@@ -6,10 +6,21 @@ import { useParams } from 'react-router-dom';
 import { getProductDetails } from '../../actions/productAction';
 import Loader from '../layout/loader/Loader';
 import { Rating } from "@material-ui/lab";
+import {
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Button,
+  } from "@material-ui/core";
 import "./productDetails.css"
+import ReviewCard from './ReviewCard';
 
 const ProductDetails = () => {
+    const[rating, setRating] = useState(0)
+    const[open, setOpen] = useState(false)
     const[quantity, setQuantity] = useState(1)
+    const[comment, setComment] = useState("")
     const dispatch = useDispatch()
     const params = useParams()
     const {loading, product, error} = useSelector((state) => state.productDetails)
@@ -23,7 +34,20 @@ const ProductDetails = () => {
     }
 
     const decreaseQuantity = () => {
-        setQuantity(quantity > 1 ? quantity -1 : quantity = 1)
+        let qty =1;
+        setQuantity(quantity > 1 ? quantity -1 : qty)
+    }
+
+    const addToCartHandler = () => {
+        dispatch()
+    }
+
+    const submitReviewToggle = () => {
+
+    }
+
+    const reviewSubmitHandler = () => {
+        
     }
 
     useEffect(() => {
@@ -47,7 +71,7 @@ const ProductDetails = () => {
                     product.images && 
                     product.images.map((item,i) => (
                         <img src = {item.url}  
-                            className = "CarouselImage"
+                            className = "carouselImage"
                             alt = {`${i} Slide`}
                             key = {i}
                                 />
@@ -72,13 +96,10 @@ const ProductDetails = () => {
                         <input readOnly type="number" value={quantity}  />
                         <button onClick = {increaseQuantity} >+</button>
                     </div>
-                    <button
-                         disabled={product.Stock < 1 ? true : false}
-                       
-                         >
+                    <button disabled={product.Stock < 1 ? true : false} 
+                            onClick = {addToCartHandler}>
                     Add to Cart
                     </button>
-
                 </div>
                 <p>
                   Status:
@@ -102,6 +123,49 @@ const ProductDetails = () => {
 
     </div>
     <h3 className="reviewsHeading">REVIEWS</h3>
+    {/* <Dialog
+            aria-labelledby="simple-dialog-title"
+            open={open}
+            onClose={submitReviewToggle}
+    >
+        <DialogTitle>Submit Review</DialogTitle>
+        <DialogContent className="submitDialog">
+            <Rating 
+                onChange = {(e) => {setRating(e.target.value)}}
+                value = {rating}
+                size = "large"
+            />
+            <textarea
+                className="submitDialogTextArea"
+                cols="30"
+                rows="5"
+                onChange = {(e) => {setComment(e.target.value)}}
+                value = {comment}
+            >
+            </textarea>
+        </DialogContent>
+        <DialogActions>
+              <Button onClick={submitReviewToggle} color="secondary">
+                Cancel
+              </Button>
+              <Button onClick={reviewSubmitHandler} color="primary">
+                Submit
+              </Button>
+        </DialogActions>
+
+
+    </Dialog> */}
+    {product.reviews && product.reviews[0] ? (
+        <div className="reviews">
+            {
+                product.reviews.map((review) => (
+                    <ReviewCard key = {review._id} review = {review} />
+                ))
+            }
+        </div>
+    ) : (
+        <div className='noReviews'>No Reviews Yet </div>
+    )}
 
     </>
   )
