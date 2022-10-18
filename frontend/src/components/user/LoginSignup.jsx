bd0ccf16112e8b4b945a80557440b3d53db51dc8
irshathahamed21 <21irshathkhan@gmail.com>
2022-10-18 13:21:22 +0530

    added login form

--- frontend/src/components/user/LoginSignup.jsx
+++ frontend/src/components/user/LoginSignup.jsx
@@ -32,6 +32,24 @@ const LoginSignup = ( {history, location}) => {
     const [avatar, setAvatar] = useState("./Profile.png")
     const [avatarPreview, setAvatarPreview] = useState("./Profile.png")
 
+    const loginSubmit = (e) => {
+        e.preventDefault()
+
+
+    }
+
+    const registerSubmit = (e) => {
+        e.preventDefault()
+
+        const myForm = new FormData()
+
+        myForm.set("name", name)
+        myForm.set("email", email)
+        myForm.set("password", password)
+        myForm.set("avatar", avatar)
+
+    }
+
     const registerDataChange = (e) => {
         if(e.target.name === "avatar") {
             const reader = new FileReader()
@@ -47,6 +65,17 @@ const LoginSignup = ( {history, location}) => {
         }
     }
 
+    const switchTabs = (e,tab) => {
+        if(tab === "login"){
+
+        }
+        if(tab === "register") {
+            
+        }
+    }
+
+    
+
 
     
   return (
@@ -60,7 +89,7 @@ const LoginSignup = ( {history, location}) => {
             </div>
             <button></button>
         </div>
-        <form className="loginForm">
+        <form className="loginForm"  encType="multipart/form-data" ref = {loginTab} onSubmit = {loginSubmit}>
             <div className="loginEmail">
                 <MailOutlineIcon />
                 <input  
@@ -84,14 +113,15 @@ const LoginSignup = ( {history, location}) => {
             <Link to="/password/forgot">Forget Password ?</Link>
             <input type="submit" value="Login" className="loginBtn" />
         </form>
-        <form>
+        <form className = "signupForm" ref = {registerTab} onSubmit = {registerSubmit}>
             <div className="signupName">
                 <FaceIcon/>
-                <input type="text"
-                placeholder='Name'
-                required
-                value = {name}
-                onChange = {registerDataChange}
+                <input 
+                    type="text"
+                    placeholder='Name'
+                    required
+                    value = {name}
+                    onChange = {registerDataChange}
                  />
             </div>
             <div className="signupEmail">
@@ -100,6 +130,8 @@ const LoginSignup = ( {history, location}) => {
                     type = "email" 
                     placeholder="Email"
                     required
+                    value = {email}
+                    onChange = {registerDataChange}
                 />
             </div>
             <div className="signupPassword">
@@ -108,16 +140,20 @@ const LoginSignup = ( {history, location}) => {
                     type = "password" 
                     placeholder="Password"
                     required
+                    value = {password}
+                    onChange = {registerDataChange}
                   />
             </div>
             <div id="registerImage">
                 <img src = {""} alt = "Avatar Preview" />
-                <input type="file" 
-                name='avatar'
-                accept = "image/*"
+                <input  
+                    type="file" 
+                    name='avatar'
+                    accept = "image/*"
+                    onChange = {registerDataChange}
                  />
             </div>
-            <input type="submit" value="Register" />
+            <input type="submit" value="Register" className='signUpBtn' />
         </form>
     </div>
 
