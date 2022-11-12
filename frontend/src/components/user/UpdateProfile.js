fc3d383684853dc65b06e2efa809674ce2ef11e3
irshathahamed21 <21irshathkhan@gmail.com>
2022-11-12 22:29:04 +0530

    UpdateProfile component done 85%

--- frontend/src/components/user/UpdateProfile.js
+++ frontend/src/components/user/UpdateProfile.js
@@ -1,22 +1,77 @@
 import React, {useEffect, useState} from 'react'
 import "./updateProfile.css"
 import {useDispatch, useSelector} from "react-redux"
-import {useAlert} from "react=alert"
+// import {useAlert} from "react-alert"
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import FaceIcon from "@material-ui/icons/Face";
+import { loadUser, updateProfile } from '../../actions/userAction';
+import { UPDATE_PROFILE_RESET } from '../../constants/userConstants';
+import Loader from '../layout/loader/Loader';
 
 
-const UpdateProfile = () => {
+const UpdateProfile = ({history}) => {
   const dispatch = useDispatch()
-  const alert = useAlert()
+  // const alert = useAlert()
 
   const {user} = useSelector((state) => state.user)
   const {error, isUpdated, loading} = useSelector((state) => state.profile)
 
-  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [avatar, setAvatar] = useState();
+  const [avatarPreview, setAvatarPreview] = useState("/Profile.png");
+
+  const updateProfileSubmit = (e) => {
+    e.preventDefault()
+
+    const myForm = new FormData()
+    myForm.set("name",name)
+    myForm.set("email",email)
+    myForm.set("avatar", avatar)
+    dispatch(updateProfile(myForm))
+
+  }
+
+  const updateProfileDataChange = (e) => {
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setAvatarPreview(reader.result);
+        setAvatar(reader.result);
+      }
+    };
+
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
+  useEffect(() => {
+    if(user){
+      setName(user.name)
+      setEmail(user.email)
+      setAvatarPreview(user.avatar.url)
+    }
+
+   if(isUpdated){
+    // alert.success("Profile Updated Successfully");
+    dispatch(loadUser());
+
+    history.push("/account");
+    dispatch({
+      type: UPDATE_PROFILE_RESET,
+    });
+   }
+
+
+  },[user,isUpdated, dispatch, history])
+
+
   
 
   return (
+    <>
+    {
+      loading ? <Loader/> :
       <>
        <div className="updateProfileContainer">
             <div className="updateProfileBox">
@@ -67,6 +122,8 @@ const UpdateProfile = () => {
               </form>
             </div>
           </div>
+      </>
+}
       </>
   )
 }
