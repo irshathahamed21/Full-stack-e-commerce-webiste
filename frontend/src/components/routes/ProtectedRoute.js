7424175e5fee4f4190859f329bf0e82557dcb8cd
irshathahamed21 <21irshathkhan@gmail.com>
2022-11-12 22:34:22 +0530

    ProtectedRoute file created

---
+++ frontend/src/components/routes/ProtectedRoute.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {useSelector} from "react-redux"
+import {Redirect,Route} from "react-router-dom"
+
+
+
+const ProtectedRoute = ({component:Component, ...rest}) => {
+    const {loading, isAuthenticated, user} = useSelector((state) => state.user)
+
+  return (
+        <>
+        {
+          loading === false && 
+         (
+          <Route 
+          {...rest} 
+          render = {(props) => {
+              if(isAuthenticated === false){
+               
+               return <Redirect to = "/login"></Redirect>
+              }
+             
+              return <Component {...props}/>
+
+          }}
+          
+          />
+         )
+        }
+        </>
+  )
+}
+
+export default ProtectedRoute
