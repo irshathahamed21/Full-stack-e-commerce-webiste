a7188c69d485d5f9f91bbc63769c72648bc9c289
irshathahamed21 <21irshathkhan@gmail.com>
2022-11-12 22:32:05 +0530

    ProductReducer file done 60%

---
+++ frontend/src/reducers/productReducer.js
@@ -0,0 +1,50 @@
+import { ALL_PRODUCT_REQUEST, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_ERROR, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_ERROR } from "../constants/productConstants";
+
+export const productsReducer = ( state = {products:[]}, action) => {
+    
+    switch(action.type){
+        case ALL_PRODUCT_REQUEST:
+            return {
+                loading:true,
+                products:[]
+            }
+        case ALL_PRODUCT_SUCCESS:
+            return {
+                loading:false,
+                products:action.payload.products,
+                productsCount: action.payload.productsCount,
+                resultPerPage: action.payload.resultPerPage,
+                filteredProductsCount: action.payload.filteredProductsCount,
+                }
+        case ALL_PRODUCT_ERROR:
+            return {
+                loading:false,
+                error:action.payload
+            }            
+        default:
+            return state
+    }
+}
+
+export const productDetailsReducer = (state = {product:{}}, action) => {
+        switch(action.type){
+            case PRODUCT_DETAILS_REQUEST:
+                return {
+                    loading:true,
+                    ...state
+                }
+            case PRODUCT_DETAILS_SUCCESS:
+                return {
+                    loading:false,
+                    product:action.payload.product
+                }
+            case PRODUCT_DETAILS_ERROR:
+                return {
+                loading:false,
+                error:action.payload
+            }            
+            default:
+                return state
+    }        
+}
+
