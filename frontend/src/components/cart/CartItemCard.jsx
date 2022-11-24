4e5d6f6ec8837fe8a02ff9fa8accdf2e9b1f09db
irshathahamed21 <21irshathkhan@gmail.com>
2022-11-24 15:25:35 +0530

    cartitemcard component done 90%

--- frontend/src/components/cart/CartItemCard.jsx
+++ frontend/src/components/cart/CartItemCard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import "./cartItemCart.css"
 
-const CartItemCard = () => {
+const CartItemCard = ({deleteCartItems,item }) => {
   return (
-    <div>CartItemCard</div>
+    <>
+    <div className="CartItemCard" key = {item.name}>
+        <img src={item.image} alt={item.name} />
+        <div>
+        <Link to={`/product/${item.product}`}>{item.name}</Link>
+        <span>{`Price: ₹${item.price}`}</span>
+        <p onClick={() => deleteCartItems(item.product)}>Remove</p>
+      </div>
+    </div>
+    </>
   )
 }
 
