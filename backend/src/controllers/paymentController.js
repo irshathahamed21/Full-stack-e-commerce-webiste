2543247847717a0a193a563617aee1de4ef0c113
irshathahamed21 <21irshathkhan@gmail.com>
2022-11-26 17:49:08 +0530

    process payment and send stripeapikey controllers created

---
+++ backend/src/controllers/paymentController.js
@@ -0,0 +1,28 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+exports.processPayment = async(req,res) => {
+    try {
+        const myPayment = await stripe.paymentIntents.create({
+            amount:req.body.amount,
+            currency:"inr",
+            metdata:{
+                company:"Ecommerce"
+            }
+        }) 
+        res.status(200).json({success:true, client_secret:myPayment.client_secret})
+
+    }
+    catch(error){
+        res.status(400).json({success:false, error:error})
+    }
+}
+
+exports.sendStripeApiKey = async(req,res) => {
+    try {
+        res.status(200).json({stripeApiKey:process.env.STRIPE_API_KEY})
+    }
+    catch(error){
+        res.status(400).json({success:false, error:error})
+
+    }
+}
