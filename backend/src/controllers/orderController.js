81cb20b67803893f4421a89c97c1b6dc185a64a4
irshathahamed21 <21irshathkhan@gmail.com>
2022-11-30 11:03:00 +0530

    logged user orders controller added

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -31,16 +31,63 @@ exports.newOrder = async(req,res) => {
       });
 }
 
-exports.getSingleOrder = async (req, res) => { 
-  const order = await Order.findById(req.params.id).populate("user", "name email")
+// get Single Order
+exports.getSingleOrder = async (req, res) => {
   
-  if(!order){
-    res.status(404).json({success:false, message:"Order not found with this Id"})
+  try {
+    const order = await Order.findById(req.params.id).populate("user", "name email")
+  
+    if(!order){
+      res.status(404).json({success:false, message:"Order not found with this Id"})
+    }
+  
+    res.status(200).json({
+      success:true,
+      order
+    })
   }
-
-  res.status(200).json({
-    success:true,
-    order
-  })
+  catch(error){
+    res.status(400).json({success:false,message:error.data.message})
+  }
+  
 
 }
+
+// get logged in user's all orders
+
+exports.myOrders = async(req,res) => {
+  try{
+    const orders = await Order.find({user:req.user._id})
+    res.status(200).json({success:true,orders})
+
+    
+  }
+  catch(error){
+    res.status(400).json({success:false,message:error.data.message})
+
+  }
+}
+
+// get all orders only for admin
+
+exports.getAllOrders = async(req,res) => {
+  try {
+    const allOrders = await Order
+
+    let totalAmount = 0;
+    allOrders.forEach((item) => {
+      totalAmount = totalAmount + item.totalPrice
+    })
+
+    res.status(200).json({success:true, allOrders})
+  }
+  catch(error){
+    res.status(400).json({success:false,message:error.data.message})
+
+  }
+}
+
+
+
+
+
