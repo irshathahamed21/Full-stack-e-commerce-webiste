e2c0f11930e12bdc5d3a7d273516327f3e0416a3
irshathahamed21 <21irshathkhan@gmail.com>
2022-11-30 11:45:55 +0530

    order routes added

--- backend/src/routes/orderRoute.js
+++ backend/src/routes/orderRoute.js
@@ -1,8 +1,18 @@
 const express = require("express")
-const { newOrder } = require("../controllers/orderController")
-const { isAuthencticatedUser } = require("../middleware/auth")
+const { newOrder, myOrders, getSingleOrder, getAllOrders, updateOrder, deleteOrder } = require("../controllers/orderController")
+const { isAuthencticatedUser, authorizeRoles } = require("../middleware/auth")
 const router = express.Router()
 
 router.route("/order/new").post(isAuthencticatedUser, newOrder)
+
+router.route("/orders/me").get(isAuthencticatedUser, myOrders)
+
+router.route("/order/:id").get(isAuthencticatedUser, getSingleOrder)
+
+router.route("/admin/orders").get(isAuthencticatedUser, authorizeRoles("admin"), getAllOrders)
+
+router.route("/admin/order/:id").put(isAuthencticatedUser, authorizeRoles("admin"), updateOrder)
+.delete(isAuthencticatedUser, authorizeRoles("admin"), deleteOrder)
+
 
 module.exports = router
