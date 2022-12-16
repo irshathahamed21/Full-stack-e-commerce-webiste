ba7cbbb885e784c00909c902cbf6442ad08607ed
irshathahamed21 <21irshathkhan@gmail.com>
2022-12-16 11:35:36 +0530

    createProduct function created for admin

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -7,9 +7,25 @@ const catchAsyncError = require("../middleware/catchAsyncError")
 const ApiFeatures = require("../middleware/apiFeatures")
 
 exports.createProducts = async (req, res, next) => {
+    try {
+        req.body.user = req.user.id
+        const product = await Product.create(req.body)
+        res.status(201).json({
+            success: true,
+            product
+        })
+    }
+    catch (err) {
+        console.log("error", err)
+    }
+
+}
 
 
+// create products --> admin
 
+
+exports.createProducts = async (req, res, next) => {
     try {
         req.body.user = req.user.id
         const product = await Product.create(req.body)
@@ -220,5 +236,80 @@ exports.deleteProductReviews = async(req,res) => {
         reviews:product.reviews
     })
 }
+
+// show all products for admin
+
+exports.getAdminProducts = async(req,res) => {
+    try{
+        const products = await Product.find()
+        res.status(200).json({success:true, products })
+    }
+    catch(err){
+        res.status(500).json({
+            success:false, message:err.message
+        })
+    }
+}
+
+// create product --> admin
+
+exports.createProduct = async(req,res) => {
+    try {
+
+        let images = []
+        if(typeof(req.body.images) === "string"){
+            images.push(req.body.images)
+        }
+        else {
+            images = req.body.images;
+        }
+
+        const imagesLinks = []
+
+        for(let i = 0; i < images.length; i++){
+            const result = await cloudinary.v2.uploader.upload(images[i], {
+                folder:"products"
+            })
+        }
+        imagesLinks.push({
+            public_id:result.public_id,
+            url:result.secure_url
+        })
+        req.body.images = imagesLinks
+        req.body.user = req.user.id
+
+        const product = await Product.create(req.body)
+        res.status(201).json({success:true, product})
+    }
+    catch(error) {
+        res.status(500).json({
+            success:false, message:err.message
+        })
+    }
+} 
+
+
+
+// update product --> admin
+
+exports.updateProduct = async(req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        
+        if(!product) {
+            res.status(404).json({message:"Product not found"})
+        }
+        let images = []
+
+    }
+    catch(error){
+
+    }
+}
+
+
+
+
+
 
 
