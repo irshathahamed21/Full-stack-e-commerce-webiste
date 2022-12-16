e3af9dd1ecd4e9731e88df4d26082c9139120077
irshathahamed21 <21irshathkhan@gmail.com>
2022-12-16 13:43:37 +0530

    routes rearranged

--- backend/src/routes/productRoute.js
+++ backend/src/routes/productRoute.js
@@ -10,21 +10,22 @@ const { createProducts, getAllProducts, getProductDetails, updateProduct
     , deleteProduct,
     createProductReview,
     getAllProductReviews,
-    deleteProductReviews
+    deleteProductReviews,
+    getAdminProducts
 } = require("../controllers/productController")
 const { isAuthencticatedUser ,authorizeRoles} = require("../middleware/auth")
 
 
 router.route("/products").get(getAllProducts)
 
+router.route("/admin/products").get(isAuthencticatedUser, authorizeRoles("admin"), getAdminProducts)
+
+
 router.route("/admin/product/new").post( isAuthencticatedUser,authorizeRoles("admin"),createProducts)
 
 router.route("/admin/product/:id").put(isAuthencticatedUser,authorizeRoles("admin"),updateProduct)
 
-router.route("/product/:id").get(getProductDetails)
-
 router.route("/admin/product/:id").delete(isAuthencticatedUser,authorizeRoles("admin"),deleteProduct)
-
 
 router.route("/product/:id").get(getProductDetails)
 
