6fc37172c35f8e01392a8d284dac74278db05a48
irshathahamed21 <21irshathkhan@gmail.com>
2022-12-19 19:48:24 +0530

    order list component created

--- frontend/src/components/admin/OrderList.js
+++ frontend/src/components/admin/OrderList.js
@@ -7,13 +7,21 @@ import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
 import SideBar from "./Sidebar";
 import Sidebar from './Sidebar'
+import { deleteOrder, getAllOrders } from '../../actions/orderAction'
+import { useEffect } from 'react'
+import { clearErrors } from '../../actions/productAction'
+import { DELETE_ORDER_RESET } from '../../constants/orderConstants'
 
 
 const OrderList = ({history}) => {
     const dispatch = useDispatch()
     const {error, orders} = useSelector((state) => state.allOrders)
-    const {error:isDeletedError, isDeleted} = useSelector((state) => state.order)
+    const {error:deleteError, isDeleted} = useSelector((state) => state.order)
 
+    const deleteOrderHandler = (id) => {
+        dispatch(deleteOrder(id))
+
+    }
     const columns = [
         {field:"id", headerName:"Order ID", minWidth:300, flex:1},
         {
@@ -53,16 +61,57 @@ const OrderList = ({history}) => {
             type:"number",
             minWidth:150,
             flex:0.3,
-            sortable:false
+            sortable:false,
+            renderCell:(params) => {
+                return (
+                    <>
+                     <Link to = {`/admin/order/${params.getValue(params.id, "id")}`}>
+                     <EditIcon/>
+                     </Link>
+                     <Button onClick = {() => deleteOrderHandler(params.getValue(params.id, "id"))}>
+                     <DeleteIcon/>
+                     </Button>
+                    </>
+                )
+            }
         },
         
     ]
+
+    const rows = []
+
+    orders && 
+    orders.forEach(() => {
+        rows.push({
+            id:item._id,
+            itemsQty: item.orderItems.length,
+            amount: item.totalPrice,
+            status: item.orderStatus,
+        })
+    })
+
+    useEffect(() => {
+        if(error){
+            dispatch(clearErrors())
+        }
+        if(deleteError){
+            dispatch(clearErrors())
+        }
+        if(isDeleted){
+            history.push("/admin/orders")
+            dispatch({type:DELETE_ORDER_RESET})
+        }
+        dispatch(getAllOrders())
+
+    },[dispatch, isDeleted, error, deleteError, history])
+
     
   return (
     <>
     <div className="dashboard">
         <Sidebar/>
         <div className="productListContainer">
+
             <DataGrid
                 rows = {rows}
                 colums = {columns}
