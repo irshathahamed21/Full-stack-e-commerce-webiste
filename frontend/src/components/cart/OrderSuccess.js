eecdb9142e58ed4720e8763a965bf4e08320d452
irshathahamed21 <21irshathkhan@gmail.com>
2022-12-25 17:00:12 +0530

    all files in the home folder updated

---
+++ frontend/src/components/cart/OrderSuccess.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Typography} from "@material-ui/core"
+import {Link} from "react-router-dom"
+import CheckCircleIcon from "@material-ui/icons/CheckCircle"
+import "./orderSuccess.css"
+
+const OrderSuccess = () => {
+  return (
+    <div className = "orderSuccess">
+    <CheckCircleIcon>
+        <Typography>Your Order has been successfully placed</Typography>
+        <Link to = "/orders">My Orders</Link>
+    </CheckCircleIcon>
+    </div>
+  )
+}
+
+export default OrderSuccess
