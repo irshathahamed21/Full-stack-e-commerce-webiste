1559ddbdcd93152d7f26c5f1de25923182ce073c
irshathahamed21 <21irshathkhan@gmail.com>
2022-12-25 17:33:30 +0530

    cloudinary package imported

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 
 const app = require("./index")
 const connect = require("./src/configs/db")
+const cloudinary = require("cloudinary")
 
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
 app.listen(3333,async() => {
     await connect().then(()=> {
         console.log("db is connected")
