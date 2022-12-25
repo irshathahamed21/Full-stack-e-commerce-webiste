c56fa986fd59d32618bbab3dd0e83e75c913c9eb
irshathahamed21 <21irshathkhan@gmail.com>
2022-12-25 17:26:08 +0530

    all reducers added in the store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,20 +1,52 @@
 import {createStore, combineReducers, applyMiddleware} from "redux"
 import thunk from "redux-thunk"
 import {composeWithDevTools} from "redux-devtools-extension"
-import { productDetailsReducer, productsReducer } from "./reducers/productReducer"
-import { userReducer } from "./reducers/userReducer"
+import { newProductReducer, newReviewReducer, productDetailsReducer, productReducer, productsReducer } from "./reducers/productReducer"
+import { allUsersReducer, profileReducer, userReducer, userDetailsReducer } from "./reducers/userReducer"
+import { cartReducer } from "./reducers/cartReducer"
+import { allOrdersReducer, myOrdersReducer, newOrderReducer, orderDetailsReducer, orderReducer } from "./reducers/orderReducer"
 
 
 const reducer = combineReducers({
     products:productsReducer,
     productDetails:productDetailsReducer,
-    user:userReducer
+    user:userReducer,
+    profile:profileReducer,
+    cart:cartReducer,
+    newOrder:newOrderReducer,
+    myOrders:myOrdersReducer,
+    allOrders:allOrdersReducer,
+    order:orderReducer,
+    orderDetails:orderDetailsReducer,
+    newReview:newReviewReducer,
+    allUsers:allUsersReducer,
+    product:productReducer,
+    newProduct:newProductReducer,
+    userDetails:userDetailsReducer
 }
 )
 
+const initialState = {
+  cart: {
+ cartItems: localStorage.getItem("cartItems")
+   ? JSON.parse(localStorage.getItem("cartItems"))
+   : [],
+ shippingInfo: localStorage.getItem("shippingInfo")
+   ? JSON.parse(localStorage.getItem("shippingInfo"))
+   : {},
+}
+}
+
+
+
 const middleware = [thunk]
-const initialState = {}
+
 
 const store = createStore(reducer, initialState,composeWithDevTools(applyMiddleware(...middleware)) )
 
 export default store
+
+
+// 02:16 - 02:37 restroom
+// 02:37 - 03:00 eating snacks
+// 03:00 - 03:42 talking with family while sitting on the floor in the hall
