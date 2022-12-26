33191a4f4bb18e92469364e7008b4b4714dd7a60
irshathahamed21 <21irshathkhan@gmail.com>
2022-12-26 19:07:26 +0530

    data grid added for showing all reviews

--- frontend/src/components/admin/ProductReviews.js
+++ frontend/src/components/admin/ProductReviews.js
@@ -6,15 +6,119 @@ import { Button } from "@material-ui/core";
 import { useAlert } from 'react-alert';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { clearErrors, deleteReviews, getAllReviews } from '../../actions/productAction';
+import DeleteIcon from "@material-ui/icons/Delete";
+import Star from "@material-ui/icons/Star";
+import { DELETE_REVIEW_RESET } from '../../constants/productConstants';
 
-
-const ProductReviews = () => {
-  const {productId, setProductId} = useState("")
+const ProductReviews = ({history}) => {
+  const [productId, setProductId] = useState("")
   const dispatch = useDispatch()
+  const alert = useAlert()
   const {loading, reviews, error} = useSelector((state) => state.productReviews)
 
+  const { isDeleted, error:deleteError} = useSelector((state) => state.review)
+ 
+  const deleteReviewHandler = (reviewId) => {
+    dispatch(deleteReviews(reviewId, productId))
+  }
+
+
+  const productReviewsSubmitHandler = (e) => {
+    e.preventDefault();
+    dispatch(getAllReviews(productId))
+  };
+
   useEffect(() => {
+    if (productId.length === 24) {
+      dispatch(getAllReviews(productId))
+      
+    }
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors()) 
+    }
+
+    if (deleteError) {
+      alert.error(deleteError);
+      dispatch(clearErrors()) 
+     
+    }
+
+    if (isDeleted) {
+      alert.success("Review Deleted Successfully");
+      history.push("/admin/reviews");
+      
+      dispatch({ type: DELETE_REVIEW_RESET });
+    }
+  }, [dispatch, alert, error, deleteError, history, isDeleted, productId])
+
+
+
+  const columns = [
+    {field:"id", headerName:"Review ID", minWidth:200, flex:0.5},
+    {
+        field:"user",
+        headerName:"User",
+        minWidth:200,
+        flex:0.6,
+    },
+    {
+        field:"comment",
+        headerName:"Comment",
+        type:"number",
+        minWidth:350,
+        flex:1
+    },
+    {
+        field:"rating",
+        headerName:"Rating",
+        type:"number",
+        minWidth:180,
+        flex:0.4,
+        cellClassName:(params) => {
+          return params.getValue(params.id, "rating") > 3 ? "greenColor" :"redColor"
+
+        }
+    },
+    {
+        field:"actions",
+        headerName:"Actions",
+        type:"number",
+        minWidth:150,
+        flex:0.3,
+        sortable:false,
+        renderCell:(params) => {
+            return (
+                <>
+                <Button onClick = {deleteReviewHandler(params.getValue(params.id, "id"))} >
+                  <DeleteIcon />
+                </Button>
+                </>
+            )
+        }
+    },
     
+]
+
+
+
+  const rows = []
+
+  reviews && 
+  reviews.forEach((item) => {
+      rows.push({
+          id:item._id,
+          user: item.user,
+          comment:item.comment,
+          rating:item.rating,
+
+      })
+  })
+
+
+  useEffect(() => {
+
   },[])
   
 
@@ -24,15 +128,16 @@ const ProductReviews = () => {
       <Sidebar/>
       
       <div className="productReviewsContainer">
-        <form onSubmit = {productReviewSubmitHandler} className = "productReviewsForm">
+        <form onSubmit = {productReviewsSubmitHandler} className = "productReviewsForm">
           <h1 className="productReviewsFormHeading">ALL REVIEWS</h1>
             <div>
+              <Star/>
               <input 
                   type = "text"
                   placeholder='Product Id'
                   required
-                  value = {ProductId}
-                  onChange = {() => setProductId(e.target.value)}
+                  value = {productId}
+                  onChange = {(e) => setProductId(e.target.value)}
                   /> 
             </div>
             <Button
@@ -50,7 +155,10 @@ const ProductReviews = () => {
         <DataGrid
           rows = {rows}
           colums = {columns}
-
+          pageSize={10}
+          disableSelectionOnClick
+          className="productListTable"
+          autoHeight
         />
       </div>
 
