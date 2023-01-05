a802f8409087a30d54542ec91f594b1fc3a80c4d
irshathahamed21 <21irshathkhan@gmail.com>
2023-01-05 17:32:48 +0530

    RightNav component is created

---
+++ frontend/src/components/layout/header/RightNav.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Link} from "react-router-dom"
+import UserOptions from './UserOptions';
+
+const Ul = styled.ul`
+  list-style: none;
+  width:500px;
+  height:70px;
+  display: flex;
+  flex-flow:row nowrap;
+  justify-content: space-evenly;
+  align-items:center;
+  border:2px solid black;
+ 
+  li {
+    padding: 8px 10px;
+    height:20px;
+  }
+   a {
+      text-decoration: none;
+      color:black;
+    }
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color:white;
+    position: fixed;
+    align-items:start;
+    transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
+    top: 0;
+    right: 0;
+    height: 500px;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+     li {
+    padding: 8px 10px;
+    height:20px;
+     }
+   
+  }
+`;
+
+const RightNav = ({ open ,user}) => {
+  return (
+    <Ul open={open}>
+      <li><Link to = "/" >Home </Link></li>
+      <li><Link to = "/products" >Products </Link></li>
+      <li><Link to = "/about" >About </Link></li>
+      <li><Link to = "/contact" >Contact </Link></li> 
+     { user ?<li><UserOptions user = {user} /></li> : <li><Link to = "/login" >Sign In </Link></li>}
+    </Ul>
+  )
+}
+
+export default RightNav
