660f7769ae66edd805cdab261ad8b8b263fd4c55
irshathahamed21 <21irshathkhan@gmail.com>
2023-01-13 18:11:27 +0530

    cart action file updated

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -6,7 +6,7 @@ export const addItemsToCart = (id,quantity) => async(dispatch, getState) => {
    
     const {data} = await axios.get(`/irshath-e-commerce-store/product/${id}`)
 
-    console.log(data)
+  
     dispatch({
         type:ADD_TO_CART,
         payload:{
