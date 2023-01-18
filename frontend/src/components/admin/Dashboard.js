36210cbe7afd3208b59432105fccac7e77ca24c7
irshathahamed21 <21irshathkhan@gmail.com>
2023-01-18 13:14:49 +0530

    number of users, products , orders not coming error fixed

--- frontend/src/components/admin/Dashboard.js
+++ frontend/src/components/admin/Dashboard.js
@@ -63,7 +63,7 @@ const Dashboard = () => {
       {
         backgroundColor: ["#00A6B4", "#6800B4"],
         hoverBackgroundColor: ["#4B5000", "#35014F"],
-        data: [outOfStock, products.length- outOfStock],
+        data: [outOfStock, products ? products.length- outOfStock:0],
       },
     ],
   };
