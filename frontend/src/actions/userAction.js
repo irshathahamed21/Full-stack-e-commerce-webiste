b76b74fcfb2883bd48e2591ed4862baa0e46a2e0
irshathahamed21 <21irshathkhan@gmail.com>
2023-01-30 15:08:16 +0530

    login and register actions requests to backend was changed from  to

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -48,11 +48,10 @@ export const login = (email,password) => async (dispatch) => {
 
         const config = {headers: {"Content-Type":"application/json"}}
 
-        const {data} = await axios.post(`/irshath-e-commerce-store/login`, {email,password}, config)
+        const {data} = await axios.post("/irshath-e-commerce-store/login", {email,password}, config)
         console.log(data)
-        console.log(data.headers.get("token"))
-
-        dispatch({type:LOGIN_SUCCESS,payload:data})
+        
+        dispatch({type:LOGIN_SUCCESS, payload:data})
     }
     catch(error) {
         dispatch({type:LOGIN_FAIL,payload:error.response.data.message})
@@ -65,7 +64,7 @@ export const register = (userData) => async (dispatch) => {
         
         const config = {headers: {"Content-Type":"multipart/form-data"}}
 
-        const {data} = await axios.post(`/irshath-e-commerce-store/register`, userData, config)
+        const {data} = await axios.post("/irshath-e-commerce-store/register", userData, config)
         console.log(data)
 
         dispatch({type:REGISTER_USER_SUCCESS,payload:data})
