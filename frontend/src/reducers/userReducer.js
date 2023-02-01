28d29155b422b7e9cc6d291c1befc4d03c3a5ee1
irshathahamed21 <21irshathkhan@gmail.com>
2023-02-01 16:51:42 +0530

    token key is added in state when login action is successful

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -58,7 +58,8 @@ import {
                     ...state,
                     loading:false,
                     isAuthenticated:true,
-                    user:action.payload.user
+                    user:action.payload.user,
+                    token:action.payload.token
                 }
             case LOGOUT_SUCCESS:
                 return  {
