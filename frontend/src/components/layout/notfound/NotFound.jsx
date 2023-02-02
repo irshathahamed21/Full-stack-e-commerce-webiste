93d11d95d6aa939c57642107c7653e4450698832
irshathahamed21 <21irshathkhan@gmail.com>
2023-02-02 12:42:37 +0530

    imported css file name changed

--- frontend/src/components/layout/notfound/NotFound.jsx
+++ frontend/src/components/layout/notfound/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ErrorIcon from "@material-ui/icons/Error";
-import "./NotFound.css";
+import "./notfound.css";
 import { Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
