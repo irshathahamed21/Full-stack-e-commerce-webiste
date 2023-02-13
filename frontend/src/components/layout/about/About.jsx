2248d1df2f7e2cc40662fce95afaf9fe5b211d0f
irshathahamed21 <21irshathkhan@gmail.com>
2023-02-13 16:09:12 +0530

    About component css file updated

--- frontend/src/components/layout/about/About.jsx
+++ frontend/src/components/layout/about/About.jsx
@@ -9,8 +9,6 @@ const About = () => {
   };
   return (
     <div className="aboutSection">
-      <div></div>
-      <div className="aboutSectionGradient"></div>
       <div className="aboutSectionContainer">
         <Typography component="h1">About Us</Typography>
 
